b5f0443f107b1757d2b9b6744bbeb53d26d8f9cb
Justin Kotalik <jukotali@microsoft.com>
2022-02-25 15:05:47 -0800

    .NET 6 and standalone version of Extensibility host (#1)

---
+++ src/Extensibility.Standalone/Providers.cs
@@ -0,0 +1,27 @@
+namespace Extensibility.Standalone
+{
+    using System.Collections.Generic;
+    using Extensibility.AzureStorage;
+    using Extensibility.Kubernetes;
+    using Extensibility.Core.Contract;
+    using System;
+
+    public static class Providers
+    {
+        private static readonly IReadOnlyDictionary<string, IExtensibilityProvider> ProvidersLookup = new Dictionary<string, IExtensibilityProvider>
+        {
+            ["AzureStorage"] = new AzureStorageProvider(),
+            ["Kubernetes"] = new KubernetesProvider(),
+        };
+
+        public static IExtensibilityProvider GetProvider(string? name)
+        {
+            if (name is null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            return ProvidersLookup.TryGetValue(name, out var provider) ? provider : throw new InvalidOperationException($"Failed to find provider \"{name}\"");
+        }
+    }
+}
