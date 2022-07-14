2243315f18591c718550e4e524a8b0308ffc2b70
Ryan Nowak <nowakra@gmail.com>
2022-07-14 15:34:24 -0700

    Shrink size of Extensibility.Standalone image (#58)
    This change switches the extensibility.standalone image to use alpine
    and self-contained publishing.
    
    This has the overall effect of reducing size from 250mb -> 112mb.
    
    I did not turn on assembly trimming because there are many many warnings
    showing up from libraries.

--- src/Extensibility.Standalone/Program.cs
+++ src/Extensibility.Standalone/Program.cs
@@ -1,14 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace Extensibility.Standalone
 {
-    using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
-
     class Program
     {
         static async Task Main(string[] args)
