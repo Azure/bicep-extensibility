a7b6d5c223a8b00bd6d2867a1e08b788e1a9a3f4
Shenglong Li <shenglong.li@outlook.com>
2022-07-19 16:52:14 -0700

    Add XML documentation comments for contract

--- src/Azure.Deployments.Extensibility.Core/IExtensibilityProvider.cs
+++ src/Azure.Deployments.Extensibility.Core/IExtensibilityProvider.cs
@@ -7,12 +7,36 @@ namespace Azure.Deployments.Extensibility.Core
 
     public interface IExtensibilityProvider
     {
+        /// <summary>
+        /// Saves an extensible resource.
+        /// </summary>
+        /// <param name="request">The save operation request.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<ExtensibilityOperationResponse> SaveAsync(ExtensibilityOperationRequest request, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Previews the result of saving an extensible resource.
+        /// </summary>
+        /// <param name="request">The preview save operation request.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<ExtensibilityOperationResponse> PreviewSaveAsync(ExtensibilityOperationRequest request, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Gets an extensible resource.
+        /// </summary>
+        /// <param name="request">The get operation request.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<ExtensibilityOperationResponse> GetAsync(ExtensibilityOperationRequest request, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Deletes an extensible resource.
+        /// </summary>
+        /// <param name="request">The delete operation request.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<ExtensibilityOperationResponse> DeleteAsync(ExtensibilityOperationRequest request, CancellationToken cancellationToken);
     }
 }
