481bde52bbb08c52b524856b14d845529caf5dcf
Shenglong Li <shenglol@microsoft.com>
2024-05-02 14:36:33 -0700

    Bump NuGet package versions

--- src/Azure.Deployments.Extensibility.Core/Extensions/ExpressionExtensions.cs
+++ src/Azure.Deployments.Extensibility.Core/Extensions/ExpressionExtensions.cs
@@ -3,15 +3,15 @@
 
 using Json.Pointer;
 using System.Linq.Expressions;
-using System.Text.Json;
 
 namespace Azure.Deployments.Extensibility.Core.Extensions
 {
     public static class ExpressionExtensions
     {
-        public static JsonPointer ToJsonPointer<T>(this Expression<Func<T, object>> expression) => JsonPointer.Create(
-            JsonPointer.Create(expression).Segments
-                .Select(x => JsonNamingPolicy.CamelCase.ConvertName(x.Value))
-                .Select(PointerSegment.Create));
+        public static JsonPointer ToJsonPointer<T>(this Expression<Func<T, object>> expression) =>
+            JsonPointer.Create(expression, new PointerCreationOptions
+            {
+                PropertyNameResolver = PropertyNameResolvers.CamelCase,
+            });
     }
 }
