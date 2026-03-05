3b31b89022d555035f06323c5b4121c06d0632da
Kyle Albert <5498623+kalbert312@users.noreply.github.com>
2026-03-05 13:09:20 -0500

    Add metadata property for updated preview contract.

---
+++ src/Azure.Deployments.Extensibility.Core/V2/Models/ResourceMetadata.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Text.Json.Serialization;
+using Azure.Deployments.Extensibility.Core.Json;
+
+namespace Azure.Deployments.Extensibility.Core.V2.Models
+{
+    public record ResourceMetadata
+    {
+        [JsonConverter(typeof(OrdinalIgnoreCaseStringSetConverter))]
+        public ISet<string>? Calculated { get; set; }
+
+        [JsonConverter(typeof(OrdinalIgnoreCaseStringSetConverter))]
+        public ISet<string>? Immutable { get; set; }
+
+        [JsonConverter(typeof(OrdinalIgnoreCaseStringSetConverter))]
+        public ISet<string>? ReadOnly { get; set; }
+
+        [JsonConverter(typeof(OrdinalIgnoreCaseStringSetConverter))]
+        public ISet<string>? Unevaluated { get; set; }
+
+        [JsonConverter(typeof(OrdinalIgnoreCaseStringSetConverter))]
+        public ISet<string>? Unknown { get; set; }
+    }
+}
